oatlog/oatlog

commit 20337be82d517046aedbff3ab42dd66be65de52a

global variable codegen and HIR rework

oatlog-core/src/runtime/global_vars.rs
@@ -1,0 +1,59 @@
+use crate::runtime::{Eclass, RelationElement, UnionFind};
+
+#[derive(Default, Debug)]
+pub struct GlobalVars<T>(Vec<GlobalVar<T>>);
+
+#[derive(Copy, Clone, Debug)]
+struct GlobalVar<T> {
+    val: T,
+    new: bool,
+    next: Option<T>,
+}
+
+impl<T: RelationElement> GlobalVars<T> {
+    pub fn define(&mut self, id: usize, val: T) {
+        assert_eq!(id, self.0.len());
+        self.0.push(GlobalVar {
+            val,
+            new: true,
+            next: None,
+        });
+    }
+    pub fn set(&mut self, id: usize, val: T, merge: impl FnOnce(T, T) -> T) {
+        let x = &mut self.0[id];
+        if val == x.val {
+            return;
+        }
+        x.next = Some(merge(x.next.unwrap_or_else(|| x.val), val));
+    }
+    pub fn update_finalize(&mut self) {
+        for GlobalVar { val, new, next } in &mut self.0 {
+            if let Some(val2) = next {
+                *val = *val2;
+                *new = true;
+                *next = None;
+            } else {
+                *new = false;
+                *next = None;
+            }
+        }
+    }
+    pub fn get(&self, id: usize) -> T {
+        self.0[id].val
+    }
+    pub fn get_new(&self, id: usize) -> Option<T> {
+        let x = self.0[id];
+        x.new.then_some(x.val)
+    }
+}
+impl<T: Eclass> GlobalVars<T> {
+    pub fn update(&mut self, uf: &mut UnionFind<T>) {
+        for GlobalVar { val, new: _, next } in &mut self.0 {
+            if let Some(next) = next {
+                *next = uf.find(*next);
+            } else if *val != uf.find(*val) {
+                *next = Some(uf.find(*val));
+            }
+        }
+    }
+}

oatlog-core/src/frontend/mod.rs
@@ -12,6 +12,7 @@ use std::{
     collections::{BTreeMap, btree_map::Entry},
     fmt::Debug,
     hash::Hash,
+    num::NonZeroU64,
 };
 
 use itertools::Itertools as _;
@@ -218,14 +219,13 @@ const BUILTIN_SORTS: [(&str, &str); 3] = [
     // TODO: we could trivially add more here for all sizes of ints/floats.
     // (BUILTIN_F64, "std::primitive::f64"),
     // (BUILTIN_BOOL, "std::primitive::bool"),
-    (BUILTIN_STRING, "oatlog::runtime::IString"),
+    (BUILTIN_STRING, "runtime::IString"),
     (BUILTIN_UNIT, "std::primitive::unit"),
 ];
 
 #[derive(Debug, Clone, PartialEq)]
 struct GlobalVariableInfo {
     ty: TypeId,
-    compute: ComputeMethod,
     relation_id: RelationId,
 }
 
@@ -338,6 +338,8 @@ struct Parser {
     symbolic_rules: Vec<hir::SymbolicRule>,
     /// Rules derived from functional dependency within relations.
     implicit_rules: BTreeMap<RelationId, Vec<hir::ImplicitRule>>,
+
+    interner: crate::runtime::StringIntern,
 }
 impl Parser {
     fn new() -> Self {
@@ -354,6 +356,7 @@ impl Parser {
             type_ids: StringIds::new("type"),
             type_to_forall: BTreeMap::new(),
             types: TVec::new(),
+            interner: crate::runtime::StringIntern::new(),
         };
         for (builtin, path) in BUILTIN_SORTS {
             let _ty: TypeId = parser
@@ -373,7 +376,6 @@ impl Parser {
                 (None, None) => hir::Type::new_symbolic(*t.name),
             })
             .collect();
-        let mut interner = crate::runtime::StringIntern::new();
         hir::Theory {
             symbolic_rules: self.symbolic_rules.clone(),
             relations: self
@@ -391,25 +393,8 @@ impl Parser {
                 .iter()
                 .map(|i| i.relation_id)
                 .collect(),
-            global_compute: self
-                .global_variables
-                .iter()
-                .map(|x| match &x.compute {
-                    ComputeMethod::Literal(Literal::I64(x)) => lir::GlobalCompute::new_i64(*x),
-                    ComputeMethod::Literal(Literal::String(x)) => {
-                        lir::GlobalCompute::new_string((*x).to_owned(), &mut interner)
-                    }
-                    ComputeMethod::Function { function, args } => {
-                        lir::GlobalCompute::new_call(*function, args)
-                    }
-
-                    ComputeMethod::Literal(Literal::F64(_) | Literal::Bool(_) | Literal::Unit) => {
-                        panic!("only literal ints and strings are implemented for globals")
-                    }
-                })
-                .collect(),
-            interner,
             initial: self.initial.clone(),
+            interner: self.interner.clone(),
         }
     }
 
@@ -600,7 +585,9 @@ impl Parser {
                         ruleset: None,
                         until: None,
                     }) => {
-                        self.initial.push(lir::Initial::run(repeat));
+                        if let Some(repeat) = NonZeroU64::new(repeat) {
+                            self.initial.push(lir::Initial::run(repeat));
+                        }
                     }
                     _ => return unimplemented_msg,
                 },
@@ -695,54 +682,65 @@ impl Parser {
     /// Add new global variable, anonymous if missing name.
     /// Hashcons based on compute method.
     /// Error if name collision.
-    fn add_global(
-        &mut self,
-        name: Option<Str>,
-        ty: TypeId,
-        compute: ComputeMethod,
-    ) -> MResult<GlobalId> {
+    fn add_global_binding(&mut self, name: Str, global_id: GlobalId) -> MResult<()> {
         // Duplicate global variable names forbidden
-        if let Some(name) = name {
-            if let Entry::Occupied(entry) = self.global_variable_names.entry(name) {
-                let existing_span = entry.key().span;
-                return Err(already_defined(
-                    name.x,
-                    existing_span,
-                    name.span,
-                    "global variable",
-                ));
-            }
+        if let Entry::Occupied(entry) = self.global_variable_names.entry(name) {
+            let existing_span = entry.key().span;
+            return Err(already_defined(
+                name.x,
+                existing_span,
+                name.span,
+                "global variable",
+            ));
         }
+        // Override dummy name with real name
+        self.relations_hir_and_func[self.global_variables[global_id].relation_id]
+            .0
+            .name = &*name;
 
+        self.global_variable_names.insert(name, global_id);
+
+        Ok(())
+    }
+    fn add_global(&mut self, ty: TypeId, compute: ComputeMethod) -> MResult<GlobalId> {
         let new_id = GlobalId(self.compute_to_global.len());
         let global_id = *self
             .compute_to_global
             .entry(compute.clone())
             .or_insert_with(|| {
                 // Create relation and variable info for global variable
-                let relation_id = self.relations_hir_and_func.push((
-                    hir::Relation::global(
-                        name.map_or_else(|| &*new_id.to_string().leak(), |x| *x),
-                        new_id,
-                        ty,
-                    ),
-                    None,
-                ));
                 self.global_variables.push_expected(
                     new_id,
                     GlobalVariableInfo {
                         ty,
-                        compute,
-                        relation_id,
+                        relation_id: {
+                            let name = &*new_id.to_string().leak();
+                            self.relations_hir_and_func
+                                .push((hir::Relation::global(&name, new_id, ty), None))
+                        },
                     },
                 );
+                self.initial.push(lir::Initial::ComputeGlobal {
+                    global_id: new_id,
+                    compute: match compute {
+                        ComputeMethod::Literal(Literal::I64(x)) => lir::GlobalCompute::new_i64(x),
+                        ComputeMethod::Literal(Literal::String(x)) => {
+                            lir::GlobalCompute::new_string((*x).to_owned(), &mut self.interner)
+                        }
+                        ComputeMethod::Function { function, args } => {
+                            lir::GlobalCompute::new_call(function, &args)
+                        }
+
+                        ComputeMethod::Literal(
+                            Literal::F64(_) | Literal::Bool(_) | Literal::Unit,
+                        ) => {
+                            panic!("only literal ints and strings are implemented for globals")
+                        }
+                    },
+                });
 
                 new_id
             });
-
-        if let Some(name) = name {
-            self.global_variable_names.insert(name, global_id);
-        }
 
         Ok(global_id)
     }
@@ -838,7 +836,7 @@ impl Parser {
             Expr::Literal(spanned) => {
                 let literal = **spanned;
                 let ty = self.literal_type(literal);
-                let global_id = self.add_global(None, ty, ComputeMethod::Literal(literal))?;
+                let global_id = self.add_global(ty, ComputeMethod::Literal(literal))?;
                 variables.push((ty, Some(global_id)))
             }
             Expr::Var(spanned) => match **spanned {
@@ -962,7 +960,7 @@ impl Parser {
                     let compute = ComputeMethod::Literal(*x);
                     let ty = parser.literal_type(*x);
 
-                    (parser.add_global(None, ty, compute)?, ty)
+                    (parser.add_global(ty, compute)?, ty)
                 }
                 Expr::Var(x) => {
                     let id = parser.global_variable_names.lookup(x, "global variable")?;
@@ -1006,7 +1004,7 @@ impl Parser {
                                 .as_ref()
                                 .unwrap()
                                 .output;
-                            (parser.add_global(None, ty, compute)?, ty)
+                            (parser.add_global(ty, compute)?, ty)
                         }
                         [] => {
                             let mut err = bare_!(
@@ -1032,9 +1030,10 @@ impl Parser {
                 }
             })
         }
-        let (id, ty) = parse(self, expr)?;
-        let compute = self.global_variables[id].compute.clone();
-        assert_eq!(id, self.add_global(binding_name, ty, compute)?);
+        let (id, _ty) = parse(self, expr)?;
+        if let Some(binding_name) = binding_name {
+            self.add_global_binding(binding_name, id)?;
+        }
         Ok(())
     }
 
@@ -1151,11 +1150,7 @@ mod compile_rule {
                 Expr::Literal(x) => {
                     let global_id = self
                         .parser
-                        .add_global(
-                            None,
-                            self.parser.literal_type(**x),
-                            ComputeMethod::Literal(**x),
-                        )
+                        .add_global(self.parser.literal_type(**x), ComputeMethod::Literal(**x))
                         .unwrap();
                     let global = &self.parser.global_variables[&global_id];
                     let relation_id = global.relation_id;

oatlog-core/src/hir.rs
@@ -452,7 +452,6 @@ pub(crate) struct Theory {
     pub(crate) symbolic_rules: Vec<SymbolicRule>,
     pub(crate) implicit_rules: BTreeMap<RelationId, Vec<ImplicitRule>>,
     pub(crate) relations: TVec<RelationId, Relation>,
-    pub(crate) global_compute: TVec<GlobalId, crate::lir::GlobalCompute>,
     pub(crate) global_types: TVec<GlobalId, TypeId>,
     #[allow(unused)]
     /// NOTE: This may be useful if global variables are queried at run time

oatlog-core/src/lir.rs
@@ -8,7 +8,7 @@ use crate::{
     typed_vec::TVec,
 };
 use derive_more::From;
-use std::iter;
+use std::{iter, num::NonZeroU64};
 
 /// Data such as type and function names are technically unnecessary but used for more readable
 /// generated code. A compiler is far less performance sensitive than an interpreter (although the
@@ -27,22 +27,21 @@ pub(crate) struct Theory {
     // between HIR and LIR. We should actually implement this, not do this `Option` hack.
     pub(crate) relations: TVec<RelationId, Option<RelationData>>,
     pub(crate) rule_variables: TVec<VariableId, VariableData>,
-    pub(crate) global_compute: TVec<GlobalId, GlobalCompute>,
-    pub(crate) global_types: TVec<GlobalId, TypeId>,
+    pub(crate) global_variable_types: TVec<GlobalId, TypeId>,
     pub(crate) rule_tries: &'static [RuleTrie],
     pub(crate) initial: Vec<Initial>,
 }
-// TODO global variables could be read as relations, not a special GlobalVar struct
-// TODO computing global variables at startup should be more dynamic, support Run(steps) between inits.
 // TODO RuleAtom simplification, e.g. PremiseNew
 
 #[derive(Debug, Clone)]
 pub(crate) enum Initial {
-    Run { steps: u64 },
-    // ComputeGlobal {
-    //     id: GlobalId,
-    //     compute: GlobalCompute,
-    // },
+    Run {
+        steps: NonZeroU64,
+    },
+    ComputeGlobal {
+        global_id: GlobalId,
+        compute: GlobalCompute,
+    },
     // assert that rule matches database.
     // Check {
     //     ...
@@ -53,7 +52,7 @@ pub(crate) enum Initial {
     // }
 }
 impl Initial {
-    pub(crate) fn run(steps: u64) -> Self {
+    pub(crate) fn run(steps: NonZeroU64) -> Self {
         Self::Run { steps }
     }
     // fn global_literal(id: GlobalId, literal: Literal) -> Self {
@@ -325,8 +324,7 @@ impl Theory {
                     types,
                     relations,
                     rule_variables,
-                    global_compute,
-                    global_types,
+                    global_variable_types,
                     rule_tries,
                     initial,
                 } = self.0;
@@ -361,8 +359,7 @@ impl Theory {
                             })
                             .collect::<BTreeMap<_, _>>(),
                     )
-                    .field("global_compute", global_compute)
-                    .field("global_types", global_types)
+                    .field("global_variable_types", global_variable_types)
                     .field(
                         "rule_tries",
                         &rule_tries.iter().map(Dbg).collect::<Vec<_>>(),

oatlog-core/src/query_planning.rs
@@ -170,8 +170,7 @@ pub(crate) fn emit_lir_theory(mut theory: hir::Theory) -> (hir::Theory, lir::The
         name: theory.name,
         types,
         relations: lir_relations,
-        global_compute: theory.global_compute.clone(),
-        global_types: theory.global_types.clone(),
+        global_variable_types: theory.global_types.clone(),
         rule_variables: lir_variables,
         rule_tries: tries.leak(),
         initial: theory.initial.clone(),

oatlog-core/src/runtime/mod.rs
@@ -1,6 +1,7 @@
 //! "runtime" functions and types to be used by generated code.
 
 mod generic;
+mod global_vars;
 mod index;
 mod row;
 mod uf;
@@ -9,6 +10,7 @@ pub use crate::{
     decl_row, eclass_wrapper_ty, relation_element_wrapper_ty,
     runtime::{
         generic::{Eclass, EclassProvider, RelationElement},
+        global_vars::GlobalVars,
         index::{Index, IndexImpl, RadixSortCtx, StdSortCtx, dedup_suffix},
         row::IndexRow,
         uf::UnionFind,

commit 9c53b22e06f195d6a3c8e8809a236a94f0cedb49

Fix errors introduced in 20337be

oatlog-core/src/codegen.rs
@@ -31,7 +31,7 @@ pub fn codegen(theory: &Theory) -> TokenStream {
         })
         .collect();
 
-    let (global_variable_fields, global_variables_map, theory_initial) =
+    let (global_variable_fields, global_variables_map, theory_initial, compute_globals) =
         codegen_globals_and_initial(theory);
 
     let mut declare_rows = BTreeMap::new();
@@ -305,6 +305,11 @@ pub fn codegen(theory: &Theory) -> TokenStream {
         impl #theory_ty {
             pub fn new() -> Self {
                 let mut theory = Self::default();
+                #(#compute_globals)*
+                // not ideal, but we need to canonicalize (put globals into database) before
+                // running step(), fixed when we are able to canonicalize without running rules.
+                // see global_vars.rs
+                theory.canonicalize();
                 #(#theory_initial)*
                 theory
             }
@@ -673,7 +678,12 @@ impl CodegenRuleTrieCtx<'_> {
 
 fn codegen_globals_and_initial(
     theory: &Theory,
-) -> (Vec<TokenStream>, TVec<GlobalId, usize>, Vec<TokenStream>) {
+) -> (
+    Vec<TokenStream>,
+    TVec<GlobalId, usize>,
+    Vec<TokenStream>,
+    Vec<TokenStream>,
+) {
     fn global_compute_to_expr(
         theory: &Theory,
         global_id: GlobalId,
@@ -766,38 +776,51 @@ fn codegen_globals_and_initial(
 
     let mut map: BTreeMap<TypeId, usize> = BTreeMap::new();
     let mut assigned_indices: TVec<GlobalId, usize> = TVec::new();
+    let compute_globals: Vec<TokenStream> = theory
+        .initial
+        .iter()
+        .filter_map(|initial| {
+            Some(match initial {
+                Initial::Run { .. } => return None,
+                Initial::ComputeGlobal { global_id, compute } => {
+                    let ty = theory.global_variable_types[global_id];
+                    let ty_data = &theory.types[ty];
+
+                    let expr =
+                        global_compute_to_expr(theory, *global_id, compute, &assigned_indices);
+                    let idx = {
+                        let entry = map.entry(ty).or_default();
+                        let idx = *entry;
+                        *entry += 1;
+                        idx
+                    };
+
+                    assigned_indices.push_expected(*global_id, idx);
+                    if ty_data.is_zero_sized() {
+                        quote! {
+                            let _ = #expr;
+                        }
+                    } else {
+                        let field = ident::type_global(ty_data);
+                        quote! {
+                            theory.#field.define(#idx, #expr);
+                        }
+                    }
+                }
+            })
+        })
+        .collect();
     let theory_initial: Vec<TokenStream> = theory
         .initial
         .iter()
-        .map(|initial| match initial {
-            Initial::Run { steps } => {
-                let steps = steps.get();
-                quote! { for _ in 0..#steps { theory.step(); } }
-            }
-            Initial::ComputeGlobal { global_id, compute } => {
-                let ty = theory.global_variable_types[global_id];
-                let ty_data = &theory.types[ty];
-
-                let expr = global_compute_to_expr(theory, *global_id, compute, &assigned_indices);
-                let idx = {
-                    let entry = map.entry(ty).or_default();
-                    let idx = *entry;
-                    *entry += 1;
-                    idx
-                };
-
-                assigned_indices.push_expected(*global_id, idx);
-                if ty_data.is_zero_sized() {
-                    quote! {
-                        let _ = #expr;
-                    }
-                } else {
-                    let field = ident::type_global(ty_data);
-                    quote! {
-                        theory.#field.define(#idx, #expr);
-                    }
+        .filter_map(|initial| {
+            Some(match initial {
+                Initial::Run { steps } => {
+                    let steps = steps.get();
+                    quote! { for _ in 0..#steps { theory.step(); } }
                 }
-            }
+                Initial::ComputeGlobal { .. } => return None,
+            })
         })
         .collect();
 
@@ -815,7 +838,12 @@ fn codegen_globals_and_initial(
         })
         .collect();
 
-    (global_variable_fields, assigned_indices, theory_initial)
+    (
+        global_variable_fields,
+        assigned_indices,
+        theory_initial,
+        compute_globals,
+    )
 }
 
 fn codegen_relation(

oatlog-core/src/runtime/global_vars.rs
@@ -16,7 +16,7 @@ impl<T: RelationElement> GlobalVars<T> {
         self.0.push(GlobalVar {
             val,
             new: true,
-            next: None,
+            next: Some(val),
         });
     }
     pub fn set(&mut self, id: usize, val: T, merge: impl FnOnce(T, T) -> T) {

comparative-test/src/lib.rs
@@ -1319,8 +1319,13 @@ egglog_test!(valid_tests_compile_ok, should_compile, expect![], "(sort Math)");
 
 egglog_test!(zrocorrect, permutation_bugs, expect![], "(include \"comparative-test/additional/permutation_bugs.egg\")");
 
-egglog_test!(mismatched, quadratic, expect![[r#"
-    Add: 504 (egglog) != 532 (oatlog)
+egglog_test!(allcorrect, quadratic, expect![[r#"
+    Add: 11063
+    Mul: 16
+    Sqrt: 1
+    Sub: 2
+    Var: 3
+    Zero: 0
 "#]], r#"
 (datatype Math
     (Mul Math Math)

oatlog-core/src/expect_tests.rs
@@ -1165,6 +1165,7 @@ fn initial_exprs() {
                         theory.delta.insert_mul((tmp0, tmp1, tmp_res));
                         tmp_res
                     });
+                    theory.canonicalize();
                     theory
                 }
                 pub fn step(&mut self) -> [std::time::Duration; 2] {
@@ -1649,6 +1650,7 @@ fn codegen_bug1() {
             impl Theory {
                 pub fn new() -> Self {
                     let mut theory = Self::default();
+                    theory.canonicalize();
                     theory
                 }
                 pub fn step(&mut self) -> [std::time::Duration; 2] {
@@ -1871,6 +1873,7 @@ fn initial() {
             impl Theory {
                 pub fn new() -> Self {
                     let mut theory = Self::default();
+                    theory.canonicalize();
                     for _ in 0..42u64 {
                         theory.step();
                     }
@@ -2567,6 +2570,7 @@ fn test_primitives_simple() {
                     theory.global_string.define(0usize, IString(0u32));
                     theory.global_string.define(1usize, IString(1u32));
                     theory.global_i64.define(2usize, 0i64);
+                    theory.canonicalize();
                     theory
                 }
                 pub fn step(&mut self) -> [std::time::Duration; 2] {
@@ -3251,6 +3255,7 @@ fn triangle_join() {
             impl Theory {
                 pub fn new() -> Self {
                     let mut theory = Self::default();
+                    theory.canonicalize();
                     theory
                 }
                 pub fn step(&mut self) -> [std::time::Duration; 2] {
@@ -3749,6 +3754,7 @@ fn edgecase0() {
             impl Theory {
                 pub fn new() -> Self {
                     let mut theory = Self::default();
+                    theory.canonicalize();
                     theory
                 }
                 pub fn step(&mut self) -> [std::time::Duration; 2] {
@@ -4216,6 +4222,7 @@ fn test_into_codegen() {
             impl Theory {
                 pub fn new() -> Self {
                     let mut theory = Self::default();
+                    theory.canonicalize();
                     theory
                 }
                 pub fn step(&mut self) -> [std::time::Duration; 2] {