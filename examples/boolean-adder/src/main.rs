18860a6ff9a660da01a698e43e8f38237cae8b2e
Loke Gustafsson <lokegustafsson@pm.me>
2025-04-10 17:56:17 +0200

    add boolean-adder example

---
+++ examples/boolean-adder/src/main.rs
@@ -0,0 +1,71 @@
+oatlog::compile_egraph!(((include "oatlog-bench/input/boolean_adder.egg")));
+
+fn run(sink: &mut impl std::io::Write, iters: usize) {
+    let mut theory = Theory::new();
+
+    writeln!(sink, "running with iters={iters}").unwrap();
+
+    let mut last = 0;
+    for i in 0..iters {
+        theory.step();
+
+        let size = theory.get_total_relation_entry_count();
+        writeln!(sink, "i={i} size={size}").unwrap();
+
+        if last == size {
+            break;
+        } else {
+            last = size;
+        }
+    }
+
+    let relation_entry_count = theory
+        .get_relation_entry_count()
+        .into_iter()
+        .map(|(name, count)| format!("{name}: {count}"))
+        .collect::<Vec<String>>()
+        .join("\n");
+    writeln!(sink, "\n{}", relation_entry_count).unwrap();
+}
+
+fn main() {
+    let iters: usize = std::env::args()
+        .nth(1)
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(10);
+
+    run(&mut std::io::stdout(), iters)
+}
+
+#[test]
+fn test() {
+    let mut sink = Vec::new();
+    run(&mut sink, 10);
+    let sink = String::from_utf8(sink).unwrap();
+    expect_test::expect!([r#"
+        running with iters=10
+        i=0 size=106
+        i=1 size=241
+        i=2 size=511
+        i=3 size=727
+        i=4 size=906
+        i=5 size=1332
+        i=6 size=2374
+        i=7 size=5246
+        i=8 size=15897
+        i=9 size=75756
+
+        And: 29540
+        False: 0
+        FullAddCarry: 10
+        FullAddSum: 10
+        HalfAddCarry: 1
+        HalfAddSum: 1
+        Not: 7074
+        Or: 38850
+        True: 0
+        Var: 22
+        Xor: 248
+    "#])
+    .assert_eq(&sink);
+}
